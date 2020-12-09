fa3d1fbbbc9a4f1b67869ee1da49a16da52f8d64
fullStackOasis <fullstackdev@fullstackoasis.com>
2020-12-08 20:13:24 -0500

    Just do not log messages even in dev

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+module.exports = api => {
+	const babelEnv = api.env();
+	let x = babelEnv !== 'development';
+	console.log("x "  + x + " babelEnv " + babelEnv);
+	const plugins = [];
+	//change to 'production' to check if this is working in 'development' mode
+	//if (babelEnv !== 'development') {
+	  plugins.push(['transform-remove-console', {exclude: ['error', 'warn']}]);
+	//}
+	return {
+	  presets: ['module:metro-react-native-babel-preset'],
+	  plugins,
+	};
+  };
