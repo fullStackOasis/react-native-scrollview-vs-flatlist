b95f513cc9dabe6e4245d61aeb02b29f8fc1413f
fullStackOasis <fullstackdev@fullstackoasis.com>
2020-12-15 16:27:40 -0500

    Adding a FooterComponent for use a footer for the entire screen (not for Sections). Made TextWrapper its own component.

---
+++ FooterComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import TextWrapper from './TextWrapper';
+
+class FooterComponent extends Component {
+	render() {
+		return (
+			<TextWrapper>Hello Footerville!</TextWrapper>
+		)
+	}
+}
+
+export default FooterComponent;
---
+++ TextWrapper.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+const TextWrapper = styled.Text`
+  text-align: center;
+  background-color: black;
+  color: lightblue;
+  font-size: 30px;
+  border: 1px lightblue;
+  margin: 1px;
+`;
+
+export default TextWrapper;
