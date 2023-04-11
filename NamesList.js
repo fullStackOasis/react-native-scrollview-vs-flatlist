11030b11144691d389fffe9dbc4419983b44a446
fullStackOasis <fullstackdev@fullstackoasis.com>
2023-04-11 19:10:55 -0400

    Linting

--- NamesList.js
+++ NamesList.js
@@ -21,19 +21,18 @@ export default class NamesList extends React.Component {
   eventListener;
 
   constructor(props) {
-	super(props);
+    super(props);
     this.state = {
-		items: {},
-		searching: false,
-		header: this.props.headerData || {},
-		selected: [],
-		scrollEnabled: false,
-		headerHeight: 0,
-
-	  };
-	console.log("NamesList constructor " + JSON.stringify(this.state.header));
-	this.renderHeader = this.renderHeader.bind(this);
-	this.onHeaderLayout = this.onHeaderLayout.bind(this);
+      items: {},
+      searching: false,
+      header: this.props.headerData || {},
+      selected: [],
+      scrollEnabled: false,
+      headerHeight: 0,
+    };
+    console.log('NamesList constructor ' + JSON.stringify(this.state.header));
+    this.renderHeader = this.renderHeader.bind(this);
+    this.onHeaderLayout = this.onHeaderLayout.bind(this);
   }
 
   componentDidMount() {
@@ -41,7 +40,7 @@ export default class NamesList extends React.Component {
       'swipe-list',
       this.handleScroll
     );
-}
+  }
 
   componentWillUnmount() {
     if (this.eventListener) {
@@ -50,10 +49,8 @@ export default class NamesList extends React.Component {
   }
 
   filterList(search, props) {
-	  console.log("NamesList.filterList");
-    const { data, sortFields, headerData } = props
-      ? props
-      : this.props;
+    console.log('NamesList.filterList');
+    const { data, sortFields, headerData } = props ? props : this.props;
     let items = {};
 
     if (data) {
@@ -75,19 +72,19 @@ export default class NamesList extends React.Component {
             if (!r[key]) r[key] = [e];
             else r[key].push(e);
           }
-console.log("XXXXX");
+          console.log('XXXXX');
           return r;
         }, {});
     }
-console.log("NamesList called setState 1");
-    this.setState({ items, header: headerData, hosts: hosts  }, () => {
+    console.log('NamesList called setState 1');
+    this.setState({ items, header: headerData, hosts: hosts }, () => {
       this.resetScroll();
     });
   }
 
   // Reset the list when nothing is entered or clearing
   resetList(event) {
-	  console.log("NamesList.resetList");
+    console.log('NamesList.resetList');
     if (event.nativeEvent.selection.end === 0) {
       this.filterList();
     }
@@ -97,43 +94,43 @@ console.log("NamesList called setState 1");
     if (this.listView && this.listView.list && this.state.items.length > 0) {
       this.listView.list.scrollToIndex({
         animated: true,
-        index: 0
+        index: 0,
       });
     }
   }
 
-  handleScroll = isScrolling => {
-	console.log("NamesList called setState 2");
+  handleScroll = (isScrolling) => {
+    console.log('NamesList called setState 2');
     this.setState({ scrollEnabled: isScrolling });
   };
 
   renderHeader() {
-	//console.log("NamesList.renderHeader this.props.headerData " + JSON.stringify(this.props.headerData));
-	  //console.log("NamesList.renderHeader this.props.data " + JSON.stringify(this.props.data));
-	let z = {...this.props.headerData};
-	console.log("NamesList.renderHeader: " + JSON.stringify(z));
-	let x = {"id":11,"name":"Aaliyah","description":"Aaliyah"};
+    //console.log("NamesList.renderHeader this.props.headerData " + JSON.stringify(this.props.headerData));
+    //console.log("NamesList.renderHeader this.props.data " + JSON.stringify(this.props.data));
+    let z = { ...this.props.headerData };
+    console.log('NamesList.renderHeader: ' + JSON.stringify(z));
+    let x = { id: 11, name: 'Aaliyah', description: 'Aaliyah' };
     return this.props.headerData ? (
       <React.Fragment>
-	  	<View onLayout={this.onHeaderLayout}>
-        <ListHeader border={false} padding={true} paddingBottom={false}>
-          Top Name
-        </ListHeader>
-        <ListItem
-          {...x}
-          style={{ backgroundColor: 'black', color: 'red', height : 50 }}
-        />
-		</View>
-	  </React.Fragment>
-	) : null;
-  };
+        <View onLayout={this.onHeaderLayout}>
+          <ListHeader border={false} padding={true} paddingBottom={false}>
+            Top Name
+          </ListHeader>
+          <ListItem
+            {...x}
+            style={{ backgroundColor: 'black', color: 'red', height: 50 }}
+          />
+        </View>
+      </React.Fragment>
+    ) : null;
+  }
 
   /**
-   * 
+   *
    */
   onHeaderLayout({ nativeEvent: { layout } }) {
-	  console.log("NamesList.onHeaderLayout, height is " + layout.height);
-	  /*
+    console.log('NamesList.onHeaderLayout, height is ' + layout.height);
+    /*
     InteractionManager.runAfterInteractions(() => {
       this.alphabet.measure((x, y, w, h, px, py) => {
         this.setState({
@@ -142,130 +139,140 @@ console.log("NamesList called setState 1");
       });
 	});
 	*/
-	if (layout && this.state && (layout.height != this.state.headerHeight)) {
-		console.log("NamesList called setState 3");
-		this.setState({
-			headerHeight: layout.height
-		});
-	}
-  };
-
+    if (layout && this.state && layout.height != this.state.headerHeight) {
+      console.log('NamesList called setState 3');
+      this.setState({
+        headerHeight: layout.height,
+      });
+    }
+  }
 
   /**
    * Render an item in the list of items. Each of the items is in a section for the
    * letter that it starts with. For example, an item with the name "Ruby" will appear
    * in the section under "R".
-   * 
+   *
    * This method is called many times; like 25 times, for every
    * name in the list. See notes in SWAlphabetFlatList.handleChildLayout
-   * 
+   *
    * @param {Object} item is for example, {"name":"Ruby","description":"Ruby","id":4}
    * @param {Number} index index within each section, a number between 0 and n,
    * can be repeated for different sections of the alphabet, but only one index per
    * item within each section
    */
   renderItem({ item, index, sectionId }) {
-	  console.log("NamesList renderItem " + JSON.stringify(item));
-	/*return <ListItem
+    console.log('NamesList renderItem ' + JSON.stringify(item));
+    /*return <ListItem
 	{...item}
 	key={index}
 	selected={item.selected}
 	style={{ height : ITEM_HEIGHT, color: 'white', backgroundColor : 'navy'}}
 	/>*/
-	return <Text
-	key={index}
-	selected={item.selected}
-	style={{ height : ITEM_HEIGHT, color: 'white', backgroundColor : 'navy'}}>{item.name}</Text>
+    return (
+      <Text
+        key={index}
+        selected={item.selected}
+        style={{
+          height: ITEM_HEIGHT,
+          color: 'white',
+          backgroundColor: 'navy',
+        }}>
+        {item.name}
+      </Text>
+    );
   }
 
-	sectionItemComponent({ title, active }) {
-		console.log("title ZZZZ " + title);
-		// title is for example "A" or "J". active is not in use.
-		return <ListFilterItem key={title} title={title} active={active} />
-	}
+  sectionItemComponent({ title, active }) {
+    console.log('title ZZZZ ' + title);
+    // title is for example "A" or "J". active is not in use.
+    return <ListFilterItem key={title} title={title} active={active} />;
+  }
 
-	respondToSelect() {
-		console.log("You selected something");
-		return null;
-	}
+  respondToSelect() {
+    console.log('You selected something');
+    return null;
+  }
 
   render() {
-	let rework = this.props.rework;
-	let noalpha = this.props.noalpha;
-	let data = this.props.data || ['No Names Found'];
-	let sections = this.props.sections || null;
-	let mapNameIndexToLetterIndex = this.props.mapNameIndexToLetterIndex || {};
-	console.log("NamesList this.props.showAlpha = " + this.props.showAlpha);
-	// titles is a list like ["A", "B", "D",...]
-	let titles = Object.keys(data);
-	console.log("NamesList render Titles were: " + JSON.stringify(titles));
-	let headerHeight = this.state ? this.state.headerHeight : 0;
-	if (this.props.rework) {
-		return (
-			<React.Fragment>
-			  <SWAlphabetFlatListRework
-			  ref={ref => (this.listView = ref)}
-			  data={data}
-			  mapNameIndexToLetterIndex={mapNameIndexToLetterIndex}
-			  sections={sections}
-			  titles={titles}
-			  itemHeight={ITEM_HEIGHT}
-			  enableEmptySections={false}
-			  removeClippedSubviews={false}
-			  scrollEnabled={true}
-			  showsVerticalScrollIndicator={false}
-			  sectionHeaderHeight={ITEM_HEIGHT}
-			  stickyHeaderIndices={[0]}
-			  renderHeader={this.renderHeader}
-			  renderItem={this.renderItem}
-			  onSelect={this.respondToSelect}
-			  headerHeight={headerHeight}
-			  sectionItemComponent={this.sectionItemComponent}
-			  sectionHeaderComponent={() => (
-				<ListHeader border={false} padding={true}>
-				ABC DOES NOTHING??
-				</ListHeader>
-				)}
-			  ListHeaderComponent={this.renderHeader}
-			  style={{
-				  flex: 1
-			  }}
-			  />
-			</React.Fragment>
-		  );
-	}
-	return (
+    console.log('NamesList this.props = ' + JSON.stringify(this.props));
+    console.log('NamesList this.props.flatList = ' + this.props.flatList);
+    let rework = this.props.rework;
+    let noalpha = this.props.noalpha;
+    let data = this.props.data || ['No Names Found'];
+    let sections = this.props.sections || null;
+    let mapNameIndexToLetterIndex = this.props.mapNameIndexToLetterIndex || {};
+    console.log('NamesList this.props.showAlpha = ' + this.props.showAlpha);
+    // titles is a list like ["A", "B", "D",...]
+    let titles = Object.keys(data);
+    console.log('NamesList render Titles were: ' + JSON.stringify(titles));
+    let headerHeight = this.state ? this.state.headerHeight : 0;
+    if (this.props.rework) {
+      return (
+        <React.Fragment>
+          <SWAlphabetFlatListRework
+            ref={(ref) => (this.listView = ref)}
+            data={data}
+            mapNameIndexToLetterIndex={mapNameIndexToLetterIndex}
+            sections={sections}
+            titles={titles}
+            itemHeight={ITEM_HEIGHT}
+            enableEmptySections={false}
+            removeClippedSubviews={false}
+            scrollEnabled={true}
+            showsVerticalScrollIndicator={false}
+            sectionHeaderHeight={ITEM_HEIGHT}
+            stickyHeaderIndices={[0]}
+            renderHeader={this.renderHeader}
+            renderItem={this.renderItem}
+            onSelect={this.respondToSelect}
+            headerHeight={headerHeight}
+            sectionItemComponent={this.sectionItemComponent}
+            sectionHeaderComponent={() => (
+              <ListHeader border={false} padding={true}>
+                ABC DOES NOTHING??
+              </ListHeader>
+            )}
+            ListHeaderComponent={this.renderHeader}
+            style={{
+              flex: 1,
+            }}
+          />
+        </React.Fragment>
+      );
+    }
+    return (
       <React.Fragment>
-		<SWAlphabetFlatList
-		ref={ref => (this.listView = ref)}
-		data={data}
-		showAlpha={this.props.showAlpha}
-		titles={titles}
-		itemHeight={ITEM_HEIGHT}
-		enableEmptySections={false}
-		removeClippedSubviews={false}
-		scrollEnabled={true}
-		showsVerticalScrollIndicator={false}
-		sectionHeaderHeight={ITEM_HEIGHT}
-		stickyHeaderIndices={[0]}
-		renderHeader={this.renderHeader}
-		renderItem={this.renderItem}
-		onSelect={this.respondToSelect}
-		headerHeight={headerHeight}
-		sectionItemComponent={this.sectionItemComponent}
-		// sectionHeaderComponent is used by SWAlphabetFlatList
-		// It is not used by SWAlphabetFlatListRework
-		sectionHeaderComponent={({ title }) => {
-			return (
-			<ListHeader border={false} padding={true}>
-			{title}
-			</ListHeader>
-        )}}
-		ListHeaderComponent={this.renderHeader}
-		style={{
-			flex: 1
-		}}
-		/>
+        <SWAlphabetFlatList
+          ref={(ref) => (this.listView = ref)}
+          data={data}
+          showAlpha={this.props.showAlpha}
+          titles={titles}
+          itemHeight={ITEM_HEIGHT}
+          enableEmptySections={false}
+          removeClippedSubviews={false}
+          scrollEnabled={true}
+          showsVerticalScrollIndicator={false}
+          sectionHeaderHeight={ITEM_HEIGHT}
+          stickyHeaderIndices={[0]}
+          renderHeader={this.renderHeader}
+          renderItem={this.renderItem}
+          onSelect={this.respondToSelect}
+          headerHeight={headerHeight}
+          sectionItemComponent={this.sectionItemComponent}
+          // sectionHeaderComponent is used by SWAlphabetFlatList
+          // It is not used by SWAlphabetFlatListRework
+          sectionHeaderComponent={({ title }) => {
+            return (
+              <ListHeader border={false} padding={true}>
+                {title}
+              </ListHeader>
+            );
+          }}
+          ListHeaderComponent={this.renderHeader}
+          style={{
+            flex: 1,
+          }}
+        />
       </React.Fragment>
     );
   }
